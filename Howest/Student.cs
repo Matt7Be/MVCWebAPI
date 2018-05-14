8c71567c42abcfa16a322a120aa0f74879e7126f
Mohammed Mohammed <mohammed.mohammed@student.howest.be>
2018-05-14 17:01:50 +0200

    HowestStudent

---
+++ Howest/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Howest
+{
+    public enum Graad
+    {
+        Voldoening,
+        Onderscheiding,
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public Graad AfstudeerGraad { get; set; }
+
+        public override string ToString()
+        {
+            return $"De student met naam {Naam} is met {AfstudeerGraad} afgestudeerd.";
+        }
+
+    }
+}
